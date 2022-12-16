1a891d40e9c372fc22812c6c51c02ffbbac4fa24
kryptify <xinzhe1219@outlook.com>
2022-12-16 10:00:40 -0500

    added customer page layout

--- src/views/Customer.js
+++ src/views/Customer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // react-bootstrap components
 import {
   Badge,
@@ -14,275 +14,124 @@ import {
   OverlayTrigger,
   Tooltip,
 } from "react-bootstrap";
-import ProgressBar from "@ramonak/react-progress-bar";
-import ProductLineChart from "components/Charts/ProductLineChart";
+import { Dropdown, InputMask } from "primereact/dropdown";
 import ProductBarChart from "components/Charts/ProductBarChart";
-import ProductPieChart from "components/Charts/ProductPieChart";
+import CustomerServiceTable from "components/Table/CustomerServiceTable";
+import { Country, State, City } from 'country-state-city';
+
+const data = {
+    data: [
+      {
+        description: 0,
+        serial: "21105W0321",
+        invoice: "52635",
+        dop: "dop",
+        warranty: 1,
+        cost: 1,
+        authorised: 0
+      },
+      {
+        description: 2,
+        serial: "ADF5DS",
+        invoice: "65235",
+        dop: "dop",
+        warranty: 2,
+        cost: 2,
+        authorised: 1
+      }
+    ]
+}
+
+const countries = Country.getAllCountries()
 
 function Customer() {
+
+    const [country, setCountry] = useState(countries[1]);
+    const [state, setState] = useState(State.getAllStates(country.isoCode)[1]);
+    const [city, setCity] = useState(City.getCitiesOfState(country.isoCode, state.isoCode));
   return (
     <>
       <Container fluid>
         <Row>
-          <Col lg="3" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center">
-                      <i className="nc-icon nc-settings-tool-66 text-primary"></i>
-                    </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Total Order</p>
-                      <Card.Title as="h2">150</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-link mr-1"></i>
-                  See detail
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col lg="3" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center">
-                      <i className="nc-icon nc-check-2 text-success"></i>
-                    </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Completed Repair</p>
-                      <Card.Title as="h2">100</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-link mr-1"></i>
-                  See detail
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col lg="3" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-settings-90 text-warning"></i>
-                    </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Under Repair</p>
-                      <Card.Title as="h2">50</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-link mr-1"></i>
-                  See detail
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col lg="3" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-tag-content text-primary"></i>
-                    </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">User Comment</p>
-                      <Card.Title as="h2">635</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="fas fa-link mr-1"></i>
-                  See detail
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md="8">
+          <Col md="5">
             <Card>
               <Card.Header className="text-center">
-                <Card.Title as="h4">Customer's Orders</Card.Title>
-                <p className="card-category">1 Week performance</p>
+                <Card.Title as="h4">Customer's Information</Card.Title>
               </Card.Header>
               <Card.Body>
-                <ProductLineChart />
+                <Table borderless>
+                    <tbody>
+                        <tr>
+                            <td className="col-xs-3">ID</td>
+                            <td><Form.Control /></td>
+                        </tr>
+                        <tr>
+                            <td className="col-xs-3">Customer</td>
+                            <td><Form.Control /></td>
+                        </tr>
+                        <tr>
+                            <td className="col-xs-3">Institution</td>
+                            <td><Form.Control /></td>
+                        </tr>
+                        <tr>
+                            <td className="col-xs-3">Address</td>
+                            <td><Form.Control /></td>
+                        </tr>
+                        <tr>
+                            <td className="col-xs-3">Zip</td>
+                            <td><Form.Control /></td>
+                        </tr>
+                        <tr>
+                            <td className="col-xs-3">Country</td>
+                            <td><Dropdown className="w-100" value={country} onChange={(e) => setCountry(e.value)} options={countries} optionLabel="name" placeholder="Select country" /></td>
+                        </tr>
+                        
+                        <tr>
+                            <td className="col-xs-3">State</td>
+                            <td><Dropdown className="w-100" value={state} onChange={(e) => setState(e.value)} options={State.getStatesOfCountry(country.isoCode)} optionLabel="name" placeholder="Select state" /></td>
+                        </tr>
+                        <tr>
+                            <td className="col-xs-3">City</td>
+                            <td><Dropdown className="w-100" value={city} onChange={(e) => setCity(e.value)} options={City.getCitiesOfState(country.isoCode, state.isoCode)} optionLabel="name" placeholder="Select city" /></td>
+                        </tr>
+                        <tr>
+                            <td className="col-xs-3">Tel</td>
+                            <td><Form.Control /></td>
+                        </tr>
+                        <tr>
+                            <td className="col-xs-3">Fax</td>
+                            <td><Form.Control /></td>
+                        </tr>
+                        <tr>
+                            <td className="col-xs-3">Mobile</td>
+                            <td><Form.Control /></td>
+                        </tr>
+                        <tr>
+                            <td className="col-xs-3">Email</td>
+                            <td><Form.Control type='email'/></td>
+                        </tr>
+                        <tr>
+                            <td className="col-xs-3">Website</td>
+                            <td><Form.Control /></td>
+                        </tr>
+                    </tbody>
+                </Table>
               </Card.Body>
             </Card>
-            <Row>
-              <Col md="4">
-                <Card>
-                  <Card.Header className="text-center">
-                    <Card.Title as="h4">Lorem Ipsum</Card.Title>
-                    <hr></hr>
-                  </Card.Header>
-                  <Card.Body>
-                    <ProductPieChart />
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col md="4">
-                <Card>
-                  <Card.Header className="text-center">
-                    <Card.Title as="h4">Lorem Ipsum</Card.Title>
-                    <hr></hr>
-                  </Card.Header>
-                  <Card.Body>
-                    <ProductLineChart />
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col md="4">
-                <Card>
-                  <Card.Header className="text-center">
-                    <Card.Title as="h4">Lorem Ipsum</Card.Title>
-                    <hr></hr>
-                  </Card.Header>
-                  <Card.Body>
-                    <ProductBarChart />
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-            <Row>
-            <Col md="6">
-              <Card className="p-3">
-                <Card.Header className="text-center">
-                  <Card.Title as="h4">Products Sales</Card.Title>
-                  <p className="card-category">All products including Taxes</p>
-                </Card.Header>
-                <hr></hr>
-                <Card.Body>
-                  <div className="py-2">
-                    <p className="card-category">Product 1</p>
-                    <ProgressBar bgColor="#007bff" height="10px" borderRadius="10px" isLabelVisible={false} completed={40}/>
-                  </div>
-                  <div className="py-2">
-                    <p className="card-category">Product 2</p>
-                    <ProgressBar bgColor="#28a745" height="10px" borderRadius="10px" isLabelVisible={false} completed={80}/>
-                  </div>
-                  <div className="py-2">
-                    <p className="card-category">Product 3</p>
-                    <ProgressBar bgColor="#dc3545" height="10px" borderRadius="10px" isLabelVisible={false} completed={24}/>
-                  </div>
-                  <div className="py-2">
-                    <p className="card-category">Product 4</p>
-                    <ProgressBar bgColor="#ffc107" height="10px" borderRadius="10px" isLabelVisible={false} completed={38}/>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md="6">
-              <Card className="p-3">
-                <Card.Header className="text-center">
-                  <Card.Title as="h4">Products Sales</Card.Title>
-                  <p className="card-category">All products including Taxes</p>
-                </Card.Header>
-                <hr></hr>
-                <Card.Body>
-                  <Row>
-                    <Col xs="4" className="border-right">
-                      <p className="card-category text-right py-2">Product 1</p>
-                      <p className="card-category text-right py-2">Product 2</p>
-                      <p className="card-category text-right py-2">Product 3</p>
-                      <p className="card-category text-right py-2">Product 4</p>
-                      <p className="card-category text-right py-2">Product 5</p>
-                      <p className="card-category text-right py-2">Product 6</p>
-                    </Col>
-                    <Col xs="8">
-                      <ProgressBar className="py-2" bgColor="#007bff" baseBgColor="#ffffff" borderRadius="10px" completed={40}/>
-                      <ProgressBar className="py-2" bgColor="#28a745" baseBgColor="#ffffff" borderRadius="10px" completed={100}/>
-                      <ProgressBar className="py-2" bgColor="#dc3545" baseBgColor="#ffffff" borderRadius="10px" completed={54}/>
-                      <ProgressBar className="py-2" bgColor="#ffc107" baseBgColor="#ffffff" borderRadius="10px" completed={38}/>
-                      <ProgressBar className="py-2" bgColor="#6c757d" baseBgColor="#ffffff" borderRadius="10px" completed={75}/>
-                      <ProgressBar className="py-2" bgColor="#6610f2" baseBgColor="#ffffff" borderRadius="10px" completed={43}/>
-                    </Col>
-                  </Row>   
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
           </Col>
-          <Col md="4">
+          <Col md="7">
             <Card>
-              <Card.Header className="text-center p-5">
-                <Card.Title className="border-bottom pb-3"as="h4">Orders Statistics</Card.Title>
+              <Card.Header className="text-center pt-5">
+                <Card.Title className="border-bottom pb-3"as="h4">Service Done</Card.Title>
               </Card.Header>
               <Card.Body>
-                <div className="d-flex justify-content-between px-4 py-1">
-                  <p className="m-0">Lorem Ipsum</p>
-                  <Badge pill bg="warning" className="px-3" text="white">120</Badge>
-                </div>
-                <div className="d-flex justify-content-between px-4 py-1">
-                  <p className="m-0">product imsum</p>
-                  <Badge pill bg="warning" className="px-3" text="white">120</Badge>
-                </div>
-                <div className="d-flex justify-content-between px-4 py-1">
-                  <p className="m-0">dental product sale</p>
-                  <Badge pill bg="warning" className="px-3" text="white">120</Badge>
-                </div>
-                <div className="d-flex justify-content-between px-4 py-1">
-                  <p className="m-0">Lorem product dental save</p>
-                  <Badge pill bg="warning" className="px-3" text="white">120</Badge>
-                </div>
-                <div className="d-flex justify-content-between px-4 py-1">
-                  <p className="m-0">dental saving club</p>
-                  <Badge pill bg="warning" className="px-3" text="white">120</Badge>
-                </div>
+                <CustomerServiceTable data={data}/>
               </Card.Body>
-              <Card.Header className="text-center p-5">
-                <Card.Title className="border-bottom pb-3"as="h4">Orders Statistics</Card.Title>
+              <Card.Header className="text-center pt-5">
+                <Card.Title className="border-bottom pb-3"as="h4">Purchase Done</Card.Title>
               </Card.Header>
               <Card.Body>
-                <ProductBarChart />
-              </Card.Body>
-              <Card.Header className="text-center p-5">
-                <Card.Title className="border-bottom pb-3"as="h4">Orders Statistics</Card.Title>
-              </Card.Header>
-              <Card.Body>
-                <Row>
-                  <Col lg="6">
-                    <ProductPieChart />
-                  </Col>
-                  <Col lg="6" className="d-flex flex-column justify-content-center">
-                    <div>
-                      <p className="card-category">Product sales</p>
-                      <p className="card-category">Lorme ipsum dolor is random sentence</p>
-                      <ProgressBar bgColor="#007bff" height="10px" borderRadius="10px" isLabelVisible={false} completed={65}/>
-                    </div>
-                  </Col>
-                </Row>
+                <CustomerServiceTable data={data}/>
               </Card.Body>
             </Card>
           </Col>
